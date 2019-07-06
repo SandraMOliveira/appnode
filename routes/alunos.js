d7531249e3e7362d99ef7ba24956ba4886c0f140
SANDRA MARIA DOMINGOS DE OLIVEIRA <sandra.mdoliveira@senacsp.edu.br>
2019-07-05 22:19:35 -0300

    consulta e delete alunos

---
+++ routes/alunos.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/all', function(req, res, next) {
+    var db = require("../db");
+    db.findAll(function(err,docs){
+        if(err) return console.log(err);
+        res.render('alunos', { title: 'Alunos', alunos: docs });
+    })
+});
+
+router.get('/delete/:id', function(req, res, next){
+    var db = require("../db");
+    db.deleteAlunos(req.param.id, function(err,docs){
+        if(err) return console.log(err);
+        res.redirect('/alunos/all');
+    })
+
+})
+
+router.post('/', function(req, res, next){
+    var id = req.body.id;
+    var nome = req.body.nome;
+    var idade = req.body.idade;
+    var db = require ('--/db');
+    
+})
+module.exports = router;
